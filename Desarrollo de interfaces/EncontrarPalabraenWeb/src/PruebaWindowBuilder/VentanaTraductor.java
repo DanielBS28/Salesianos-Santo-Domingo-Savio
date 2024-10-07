3ab6213284b1c863a8b186d6ea744156af363d9e
Daniel BS <danielsalesianosdosa@gmail.com>
2024-10-07 14:21:15 +0200

    7oct

---
+++ Desarrollo de interfaces/EncontrarPalabraenWeb/src/PruebaWindowBuilder/VentanaTraductor.java
@@ -0,0 +1,83 @@
+package PruebaWindowBuilder;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import java.awt.Cursor;
+
+public class VentanaTraductor {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaTraductor window = new VentanaTraductor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaTraductor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 128, 255));
+		frame.getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("NOMBRE");
+		lblNewLabel.setBounds(113, 58, 101, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("APELLIDOS");
+		lblNewLabel_1.setBounds(113, 113, 75, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(224, 55, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(224, 110, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("CANCELAR");
+		btnNewButton.setBackground(new Color(255, 0, 0));
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBounds(54, 197, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("SIGUIENTE");
+		btnNewButton_1.setBackground(new Color(0, 128, 0));
+		btnNewButton_1.setBounds(302, 197, 89, 23);
+		frame.getContentPane().add(btnNewButton_1);
+	}
+}
--- Desarrollo de interfaces/EncontrarPalabraenWeb/src/ejercicio1.java
+++ Desarrollo de interfaces/EncontrarPalabraenWeb/src/ejercicio1.java
@@ -7,65 +7,61 @@ public class ejercicio1 {
 
 	public static void main(String[] args) throws IOException {
 
-	
 		String direccionString = "https://www.ingles.com/traductor/";
-		
+
 		System.out.println("Dime una palabra en español para traducirla");
-		Scanner teclado = new Scanner(System.in); 
-		String palabra =teclado.nextLine();
+		Scanner teclado = new Scanner(System.in);
+		String palabra = teclado.nextLine();
 		direccionString += palabra;
-				 
-		
+
 		URL direccion = new URL(direccionString);
 		String html = obtenerHTML(direccion);
-		
-		
+
 		// System.out.println(html);
 		// buscarHtml(html);
-		
+
 		buscarHtmlpro(html);
-		
+
 	}
 
 	private static void buscarHtmlpro(String html) {
-		
-		String id= "\"MhZ0VHvJ\">";
+
+		String id = "\"MhZ0VHvJ\">";
 		char bandera = '<';
 		boolean caracterBandera = false;
-		
-		int inicio = html.indexOf(id);
-		String resultado = html.substring(inicio+id.length());
-		String palabra = "";
-	
-			for(int i = 0; !caracterBandera; i++) {
-				
-				if(resultado.charAt(i) == bandera)
-					caracterBandera = true;
-				else
-					palabra+=resultado.charAt(i);
-			}
-			
-			System.out.println(palabra.toUpperCase());
-		}
-		
-	
-/*
-	private static void buscarHtml(String html) {
-		
-		String palabra = "<a href=\"/traductor/dog?langFrom=en\" class=\"MhZ0VHvJ\">";
-		
-		int inicio =  html.indexOf(palabra);
-		
-		String resultado = html.substring(inicio+54, inicio+57);
-		String resultado2 = html.substring(inicio+palabra.length(), inicio+palabra.length()+3);
 
-		
-		System.out.println(resultado);
-		System.out.println(resultado2);
-		
-	
+		int inicio = html.indexOf(id);
+		String resultado = html.substring(inicio + id.length());
+		String palabra = "";
+
+		for (int i = 0; !caracterBandera; i++) {
+
+			if (resultado.charAt(i) == bandera)
+				caracterBandera = true;
+			else
+				palabra += resultado.charAt(i);
+		}
+
+		System.out.println(palabra.toUpperCase());
 	}
-*/
+
+	/*
+	 * private static void buscarHtml(String html) {
+	 * 
+	 * String palabra =
+	 * "<a href=\"/traductor/dog?langFrom=en\" class=\"MhZ0VHvJ\">";
+	 * 
+	 * int inicio = html.indexOf(palabra);
+	 * 
+	 * String resultado = html.substring(inicio+54, inicio+57); String resultado2 =
+	 * html.substring(inicio+palabra.length(), inicio+palabra.length()+3);
+	 * 
+	 * 
+	 * System.out.println(resultado); System.out.println(resultado2);
+	 * 
+	 * 
+	 * }
+	 */
 	private static String obtenerHTML(URL direccion) throws IOException {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(direccion.openStream()));
