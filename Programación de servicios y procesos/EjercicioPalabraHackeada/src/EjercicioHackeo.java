881cc6dcb2f21c523a8a36f062ed0feb7a691acd
Daniel BS <danielsalesianosdosa@gmail.com>
2024-11-11 14:26:08 +0100

    11nov

--- Programación de servicios y procesos/EjercicioPalabraHackeada/src/EjercicioHackeo.java
+++ Programación de servicios y procesos/EjercicioPalabraHackeada/src/EjercicioHackeo.java
@@ -1,10 +1,13 @@
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.concurrent.RecursiveAction;
 
 public class EjercicioHackeo {
 
 	public static byte[] getHash(String text) {
+		
 		byte[] encodedhash = null;
 		MessageDigest digest;
 		try {
@@ -19,22 +22,40 @@ public class EjercicioHackeo {
 
 	public static boolean comprobarHash(byte[] hashPasado) {
 
-		String HashPalabraPasada = byteArrayToHex(hashPasado);
+		String HashPalabraPasada = ArrayDeBytesHexadecimal(hashPasado);
 		System.out.println(HashPalabraPasada);
-		
+
 		String hashInicial = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
-		
+
 		return HashPalabraPasada.equals(hashInicial);
 	}
 
-	public static String byteArrayToHex(byte[] bytes) {
+	public static String ArrayDeBytesHexadecimal(byte[] bytes) {
 		String palabra = "";
-		
+
 		for (byte b : bytes)
 
 			palabra += String.format("%02x", b);
 
 		return palabra;
+	}
+
+	public static boolean recursiva(String prefijo, int longitud, String hashADescifrar) {
+
+		if (prefijo.length() == longitud) {
+			if (comprobarHash(getHash(prefijo))) {
+				System.out.println(prefijo);
+				return true;
+			}
+			return false;
+		} else {
+			for (char letra = 'a'; letra <= 'z'; letra++) {
+				String nuevaPalabra = prefijo + letra;
+				if (recursiva(nuevaPalabra, longitud, hashADescifrar))
+					return true;
+			}
+		}
+		return false;
 	}
 
 	public static String generarPalabras() {
@@ -51,13 +72,12 @@ public class EjercicioHackeo {
 						palabraEncontrada = comprobarHash(getHash(palabra));
 					}
 		}
-
 		return palabra;
 	}
 
 	public static void main(String[] args) {
-
-		System.out.println(generarPalabras());
+		//System.out.println(generarPalabras());
+		recursiva("", 4, "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79");
 	}
 
 }
