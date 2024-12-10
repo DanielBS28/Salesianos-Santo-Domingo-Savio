83206eae39c81eeef4c3accaa112685fb9c4d364
Daniel BS <danielsalesianosdosa@gmail.com>
2024-12-10 14:30:28 +0100

    10dic

---
+++ Programacion multimedia y dispositivos moviles/basic-android-kotlin-compose-training-lemonade-main/app/src/main/java/com/example/lemonade/ui/theme/Lemonade.kt
@@ -0,0 +1,8 @@
+package com.example.lemonade.ui.theme
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.StateFlow
+
+class Lemonade : ViewModel(){
+    val uiState : StateFlow <CLASEPROPIA> ;
+}
---
+++ Programacion multimedia y dispositivos moviles/basic-android-kotlin-compose-training-lemonade-main/app/src/main/java/com/example/lemonade/ui/theme/LemonadeUiState.kt
@@ -0,0 +1,10 @@
+package com.example.lemonade.ui.theme
+
+data class LemonadeUiState (
+
+    val squezeCount : Int = 0;
+    val screen Int =0;
+    val squeze : Int =0;
+
+
+)
