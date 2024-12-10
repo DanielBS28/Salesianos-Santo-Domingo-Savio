332b561ae030c0d03d2ec90c6b1c37c686a1eb7f
Daniel BS <danielsalesianosdosa@gmail.com>
2024-12-10 23:24:40 +0100

    10dic

--- Programacion multimedia y dispositivos moviles/Curso - Practica 1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Programacion multimedia y dispositivos moviles/Curso - Practica 1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Magenta) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
