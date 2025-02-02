de9c9536c0b2b22b84fbeb6e5dffe6a5dbfbcfca
Daniel BS <danielsalesianosdosa@gmail.com>
2025-02-02 17:42:31 +0100

    modificación en eliminar usuarios correcta sin funciones

--- Desarrollo de interfaces/EjercicioFinalJavaSwing - Daniel Baldazo Sánchez/src/Mecanografía/PanelEliminarUsuarios.java
+++ Desarrollo de interfaces/EjercicioFinalJavaSwing - Daniel Baldazo Sánchez/src/Mecanografía/PanelEliminarUsuarios.java
@@ -7,7 +7,10 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
 
 public class PanelEliminarUsuarios extends JPanel{
 	
@@ -37,10 +40,29 @@ public class PanelEliminarUsuarios extends JPanel{
 			volverAtrás.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 15));
 			volverAtrás.setBounds(123, 469, 241, 27);
 			add(volverAtrás);
+						
+			JButton EliminarUsuario = new JButton("Eliminar usuario");
+			EliminarUsuario.setBounds(108, 392, 276, 38);
+			EliminarUsuario.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 15));
+			EliminarUsuario.setForeground(new Color(255, 255, 255));
+			EliminarUsuario.setBackground(new Color(187, 37, 30));
+			add(EliminarUsuario);
+			
+			JLabel Pregunta = new JLabel("¿Qué usuario desea eliminar?");
+			Pregunta.setHorizontalAlignment(SwingConstants.CENTER);
+			Pregunta.setForeground(Color.WHITE);
+			Pregunta.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 22));
+			Pregunta.setBounds(73, 60, 352, 65);
+			add(Pregunta);
+			
+
+			JList list = new JList();
+			list.setBounds(70, 153, 356, 184);
+			add(list);
 			
 			add(Imágenes.ponerFondo(fondoPanelLogin, frameMecanografía));
+			
 
 		}
-			
 	}
 
